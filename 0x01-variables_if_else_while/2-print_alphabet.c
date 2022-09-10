81f7298572bd988f9c8196897b8a06503763b3f2
tonybinge <ochubatonyc@gmail.com>
2022-09-10 04:27:28 +0000

    Alphabet insomnia

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 /**
- * main - Write a program that prints the alphabet in lowercase, followed by a new line
+ * main -Program that prints the alphabet in lowercase, followed by a new line
  * Description: using the main function
  * this program prints " Alphabets in lowercase, followed by a new line"
  * Return: 0
