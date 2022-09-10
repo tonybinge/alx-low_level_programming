7f1e8132c48d53c7cd24cf3473f85e9887275bcd
tonybinge <ochubatonyc@gmail.com>
2022-09-10 16:56:45 +0000

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,16 +8,16 @@
  */
 int main(void)
 {
-char c;
-for (c = '0', c <= '9', c++)
-{
-	putchar(c);
-}
-for (c = 'a', c <= 'f', c++)
-{
-	putchar(c);
-}
-putchar('\n');
-return (0);
-}
+	char c;
 
+	for (c = '0', c <= '9', c++)
+	{
+		putchar(c);
+	}
+	for (c = 'a', c <= 'f', c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
