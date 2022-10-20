8db98dedb581528a62e03231b601e2601443e48a
tonybinge <ochubatonyc@gmail.com>
2022-10-20 16:26:59 +0000

    Prints hare and tortoise before main function

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -8,6 +8,6 @@ void __attribute__((constructor)) hare(void);
 */
 void hare(void)
 {
-	printf("Your're beat! and yet, you must allow,");
-	printf("\nI bore my house upon my back!\n");
+	printf("Your're beat! and yet, you must allow,);
+		\nI bore my house upon my back!\n");
 }
